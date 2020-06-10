084952ff2ff8b1b6f16d2b05ced189ce5d9a07bc
saxenaharsh <harsh.saxena@gmail.com>
2020-06-10 23:59:13 +0530

    Update Main.java

--- BankAccount/src/com/HarshSaxena/Main.java
+++ BankAccount/src/com/HarshSaxena/Main.java
@@ -7,18 +7,19 @@ public class Main {
 
 
 
-        BankAccount harshsaccount = new BankAccount();
+           BankAccount harshsaccount = new BankAccount();//(12345, 0.00, "Harsh Saxena", "harsh.saxena@gmail.com",
+               // "7798169988");
 
-        harshsaccount.setAccountNumber(12345);
-        harshsaccount.setCustomerName("Harsh");
-        harshsaccount.setEmail("harsh.saxena@gmail.com");
-        harshsaccount.setPhoneNumber("7798169988");
+
 
         harshsaccount.withdrawal(100.0);
         harshsaccount.deposit(50.0);
         harshsaccount.withdrawal(100.0);
         harshsaccount.deposit(51);
         harshsaccount.withdrawal(100.0);
+
+        BankAccount lamusAccount = new BankAccount("Lamu", "lamu@gmail.com", "12335");
+        System.out.println(lamusAccount.getAccountNumber() + " name " + lamusAccount.getCustomerName());
 
 
 
